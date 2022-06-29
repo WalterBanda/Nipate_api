49e0de7e22ee5e631ebc1bf41c9e29fc3867141d
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-06-29 05:23:28 +0300

    ch(services): services endpoints refactor

--- lbs_backend/locations/views.py
+++ lbs_backend/locations/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from locations.models import TownsModel, CountyModel
 from locations.serializers import TownsModelSerializer, CountyModelSerializers
@@ -15,7 +17,16 @@ class CountyView(APIView):
 
 class TownsView(APIView):
 
+    CountyID = openapi.Parameter('CountyID', openapi.IN_QUERY, description="Search by CountyID(optional)", type=openapi.TYPE_INTEGER)
+    @swagger_auto_schema(operation_description="Endpoint for searching town loactions", manual_parameters=[CountyID], responses={200: TownsModelSerializer(many=True)})
     def get(self, request):
+        data = request.query_params
+        try:
+            towns = TownsModel.objects.filter(CountID_id=data["CountyID"])
+            serializer = TownsModelSerializer(towns, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
         towns = TownsModel.objects.all()
         serializer = TownsModelSerializer(towns, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
--- lbs_backend/services/views/requestView.py
+++ lbs_backend/services/views/requestView.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,55 +14,61 @@ from services.serializers.serializer_forms import (
     ServiceRequestCreationSerializer, CreateServiceProviderSerilizer
 )
 
+
+# Service Provider CRUD Endpoint
+
 class ServiceProviderView(APIView):
     ID = openapi.Parameter('ProviderID', openapi.IN_QUERY, description="Get by ProviderID param(optional)", type=openapi.TYPE_INTEGER)
     ProductID = openapi.Parameter('ProductID', openapi.IN_QUERY, description="Get by ProductID param(optional)", type=openapi.TYPE_INTEGER)
-    @swagger_auto_schema(operation_description="Endpoint for Providers get response", manual_parameters=[ID, ProductID], responses={200: ServiceProviderSerializer(many=True)})
+    @swagger_auto_schema(operation_description="Endpoint for Service Providers get response", manual_parameters=[ID, ProductID], responses={200: ServiceProviderSerializer(many=True)})
     def get(self, request):
+        data = request.query_parms
         try:
-            data = request.query_parms
-            try:
-                try:
-                    provider = ServiceProvider.objects.filter(id=data["dataID"], ProductID_id=data["ProductID"])
-                    serializer = ServiceProviderSerializer(provider, many=True)
-                    return Response(serializer.data, status=status.HTTP_200_OK)
-                except ServiceProvider.DoesNotExist:
-                    pass
-            except:
-                if data["ProductID"]:
-                    provider = ServiceProvider.objects.filter(ProductID_id=data["ProductID"])
-                    serializer = ServiceProviderSerializer(provider, many=True)
-                    return Response(serializer.data, status=status.HTTP_200_OK)
-                else:
-                    pass
-        except:
-            providers = ServiceProvider.objects.all()
-            serializer = ServiceProviderSerializer(providers, many=True)
+            provider = ServiceProvider.objects.filter(id=data["dataID"], ProductID_id=data["ProductID"])
+            serializer = ServiceProviderSerializer(provider, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        except ServiceProvider.DoesNotExist:
+            pass
+        
+        try:
+            provider = ServiceProvider.objects.filter(ProductID_id=data["ProductID"])
+            serializer = ServiceProviderSerializer(provider, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+        
+        providers = ServiceProvider.objects.all()
+        serializer = ServiceProviderSerializer(providers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
 
-    @swagger_auto_schema(
+    @swagger_auto_schema(operation_description="Endpoint for Service Providers Creation",
         request_body=CreateServiceProviderSerilizer,responses={200: ServiceProviderSerializer(many=False)}
     )
     def post(self, request):
-        try:
-            data = request.data
-            provider = ServiceProvider(UserID_id=data["UserID"], ProductID_id=data["ProductID"])
-            if data["LocationID"] is not None:
-                provider.LocationID_id = data["LocationID"]
 
+        data = request.data
+        serializer = CreateServiceProviderSerilizer(data=data)
+        if serializer.is_valid():
+            provider = ServiceProvider(UserID_id=data["UserID"], ProductID_id=data["ProductID"], LocationID_id=data["LocationID"])
             if data["GenderID"] is not None:
                 provider.GenderID_id = data["GenderID"]
-
             if data["AgeBracket"] is not None:
                 provider.AgeBracket = data["AgeBracket"]
+            try:
+                provider.save()
+                serializer = ServiceProviderSerializer(provider, many=False)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except IntegrityError:
+                return Response(
+                    {"Error": ["Db UserID or ProductID or LocationID or GenderID IntegrityError constraint failed"]},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            provider.save()
-            serializer = ServiceProviderSerializer(provider, many=False)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
 
+# Services CRUD Endpoint
 
 class RequestServiceView(APIView):
 
@@ -70,33 +77,52 @@ class RequestServiceView(APIView):
     LocationID = openapi.Parameter('LocationID', openapi.IN_QUERY, description="Get by LocationID param(optional)", type=openapi.TYPE_INTEGER)
     @swagger_auto_schema(operation_description="Endpoint for Services Requests", manual_parameters=[UserID, ProductID, LocationID], responses={200: RequestedServiceSerializer(many=True)})
     def get(self, request):
+        
+        data = request.query_params
         try:
-            data = request.query_params
-            try:
-                requests = ServiceRequest.objects.filter(ProductID_id=data["ProductID"], UserID_id=data["UserID"], LocationID_id=data["LocationID"])
-                serializer = RequestedServiceSerializer(requests, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except:
-                pass
-            if data["UserID"] is not None:
-                requests = ServiceRequest.objects.filter(UserID_id=data["UserID"])
-                serializer = RequestedServiceSerializer(requests, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
-            if data["LocationID"] is not None:
-                requests = ServiceRequest.objects.filter(LocationID_id=data["LocationID"])
-                serializer = RequestedServiceSerializer(requests, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
-            if data["ProductID"] is not None:
-                requests = ServiceRequest.objects.filter(ProductID_id=data["ProductID"])
-                serializer = RequestedServiceSerializer(requests, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-
-        except:
-            requests = ServiceRequest.objects.all()
+            requests = ServiceRequest.objects.filter(ProductID_id=data["ProductID"], UserID_id=data["UserID"], LocationID_id=data["LocationID"])
             serializer = RequestedServiceSerializer(requests, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+        try:
+            requests = ServiceRequest.objects.filter(UserID_id=data["UserID"], ProductID_id=data["ProductID"])
+            serializer = RequestedServiceSerializer(requests, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+        
+        try:
+            requests = ServiceRequest.objects.filter(UserID_id=data["UserID"], LocationID_id=data["LocationID"])
+            serializer = RequestedServiceSerializer(requests, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+
+        try:
+            requests = ServiceRequest.objects.filter(UserID_id=data["UserID"])
+            serializer = RequestedServiceSerializer(requests, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+        
+        try:
+            requests = ServiceRequest.objects.filter(LocationID_id=data["LocationID"])
+            serializer = RequestedServiceSerializer(requests, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+
+        try:
+            requests = ServiceRequest.objects.filter(ProductID_id=data["ProductID"])
+            serializer = RequestedServiceSerializer(requests, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            pass
+        
+        requests = ServiceRequest.objects.all()
+        serializer = RequestedServiceSerializer(requests, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @swagger_auto_schema(
         request_body= ServiceRequestCreationSerializer,responses={201: RequestedServiceSerializer(many=False)}
@@ -106,12 +132,18 @@ class RequestServiceView(APIView):
         serializer = ServiceRequestCreationSerializer(data=data)
         if serializer.is_valid():
             requests = ServiceRequest(
-                ProductID_id=data["ProductID"], UserID_id=["UserID"], LocationID_id=data["LocationID"]
+                ProductID_id=data["ProductID"], UserID_id=data["UserID"], LocationID_id=data["LocationID"]
             )
             if data["RequestText"] is not None:
                 requests.RequestText = data["RequestText"]
-            requests.save()
-            serializer = RequestedServiceSerializer(requests, many=False)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            try:
+                requests.save()
+                serializer = RequestedServiceSerializer(requests, many=False)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except IntegrityError:
+                return Response(
+                    {"Error": ["Db ProductID or UserID or LocationID IntegrityError constraint failed"]},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
         else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
