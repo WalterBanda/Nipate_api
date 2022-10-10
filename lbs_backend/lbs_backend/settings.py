5bb744a450b56f558a95113301b6557912bdb3ce
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-10-10 12:15:06 +0300

    chore: Update Config settings

--- lbs_backend/lbs_backend/settings.py
+++ lbs_backend/lbs_backend/settings.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-dotenv_path = os.path.join(BASE_DIR, ".env")
-load_dotenv(dotenv_path)
+# dotenv_path = os.path.join(BASE_DIR, ".env")
+# load_dotenv(dotenv_path)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
