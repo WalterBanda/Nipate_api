44df49d0c761f279ff64aeebb4650a9ed9c6b2d0
Dev Banda <wbanda@kabarak.ac.ke>
2022-10-27 17:22:46 +0300

    ☕ Updated serializer for user model

--- lbs_backend/users/serializers.py
+++ lbs_backend/users/serializers.py
@@ -18,7 +18,7 @@ class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = [
-            "id", "MobileNumber", "IDNumber", "FirstName", "SurName"
+            "id", "mobileNumber", "idNumber", "firstName", "surName"
         ]
 
     @staticmethod
@@ -35,18 +35,18 @@ class UserModelSerializer(serializers.ModelSerializer):
 
 
 class userDetailsValidationSerializer(serializers.Serializer):
-    MobileNumber = serializers.CharField(max_length=12,
+    mobileNumber = serializers.CharField(max_length=12,
                                          min_length=12, error_messages={'blank': 'invalid mobile number',
                                                                         'length': 'invalid mobile number'})
-    IDNumber = serializers.IntegerField()
-    FirstName = serializers.CharField(max_length=50)
-    SurName = serializers.CharField(max_length=50)
+    idNumber = serializers.IntegerField()
+    firstName = serializers.CharField(max_length=50)
+    surName = serializers.CharField(max_length=50)
 
 
 class userPutDetailSerializer(serializers.Serializer):
     UserID = serializers.IntegerField()
-    LocationID = serializers.IntegerField()
-    GenderID = serializers.IntegerField()
+    locationID = serializers.IntegerField()
+    genderID = serializers.IntegerField()
     password = serializers.CharField(min_length=8)
 
 
@@ -55,14 +55,14 @@ class tokenSerializer(serializers.Serializer):
 
 
 class CreateAuthToken(serializers.Serializer):
-    MobileNumber = serializers.CharField(max_length=12, min_length=10)
+    mobileNumber = serializers.CharField(max_length=12, min_length=10)
     password = serializers.CharField(min_length=4)
 
 
 class LoginResponseSerializer(serializers.Serializer):
-    MobileNumber = serializers.CharField()
-    FirstName = serializers.CharField()
-    LastName = serializers.CharField()
+    mobileNumber = serializers.CharField()
+    firstName = serializers.CharField()
+    lastName = serializers.CharField()
     Auth_token = serializers.CharField()
 
 
@@ -75,6 +75,6 @@ class AllDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = [
-            "id", "MobileNumber", "IDNumber", "FirstName", "SurName",
-            "Location", "Gender"
+            "id", "mobileNumber", "idNumber", "firstName", "surName",
+            "location", "gender"
         ]
